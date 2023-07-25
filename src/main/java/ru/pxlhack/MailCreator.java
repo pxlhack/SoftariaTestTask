deda82846e6e29af71f3d5999a2994e50e80e7f7
pxlhack <nellsouhd@gmail.com>
2023-07-25 17:08:10 +0700

    added MailCreator

---
+++ src/main/java/ru/pxlhack/MailCreator.java
@@ -0,0 +1,11 @@
+package ru.pxlhack;
+
+import java.util.Map;
+
+public class MailCreator {
+
+    public String create(Map<String, String> todaysWebsiteState,
+                         Map<String, String> yesterdaysWebsiteState) {
+        return null;
+    }
+}
